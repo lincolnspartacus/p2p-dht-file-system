54dfdb12e104a1b4ed3ef97535fb6686b2db5eae
Madhav Kanbur <abcdjdj@gmail.com>
2022-04-26 19:18:04 -0500

    Try RPC in python
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

---
+++ test_chord.py
@@ -0,0 +1,15 @@
+import grpc
+import chord_pb2_grpc
+import chord_pb2
+
+def main():
+    print("Trying to reach the chord server..")
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = chord_pb2_grpc.ChordServiceStub(channel)
+
+    request = chord_pb2.FindSuccessorRequest(id = 5235)
+    response = stub.findSuccessor(request)
+    print(f"Response address = {response.addr}")
+
+if __name__ == "__main__":
+    main()
