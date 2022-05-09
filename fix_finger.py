450d7b069d730640376a38d8bdd51d6abdcdc4d8
Madhav Kanbur <abcdjdj@gmail.com>
2022-05-09 01:01:49 -0500

    fix_finger : Update entire list in one shot
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- fix_finger.py
+++ fix_finger.py
@@ -13,7 +13,6 @@ class FixFinger(Thread):
     def __init__(self, node):
         Thread.__init__(self)
         self.node = node
-        self.next_finger = 0
         self.ring_modulo = pow(2, self.node.ring_bits)
 
     def run(self):
@@ -26,18 +25,17 @@ class FixFinger(Thread):
             sleep_time = random.randint(low, high)
             time.sleep(sleep_time)
 
-            self.next_finger = (self.next_finger + 1) % self.node.ring_bits
-            if self.next_finger == 0: # Skip finger_table[0] because it is set elsewhere by Chord
-                continue
-            (id, ip) = self.node.ftable[self.next_finger]
-            print('[fix_finger] {}: CurrentID {} CurrentNodeIP {}'.format(self.node.id, id, ip))
-            id = self.node.id + pow(2, self.next_finger) 
-            id = id % self.ring_modulo
+            for next_finger in range(1, self.node.ring_bits): # Skip finger_table[0] because it is set elsewhere by Chord
+                (id, ip) = self.node.ftable[next_finger]
+                print('[fix_finger] {}: CurrentID {} CurrentNodeIP {}'.format(self.node.id, id, ip))
+                id = self.node.id + pow(2, next_finger)
+                id = id % self.ring_modulo
+
+                # Successor will most likely be consistent because of stabilize thread. So route to it
+                (suc_id, target_ip) = self.node.get_successor()
+                #print('[fix_finger] {}: Target CurrentID {} CurrentNodeIP {}'.format(self.node.id, id, target_ip))
+                (id, ip) = self.node.find_successor(id, target_ip)
+                self.node.ftable[next_finger] = (id, ip)
+                print('[fix_finger] {}: UpdatedID {} UpdatedNodeIP {}'.format(self.node.id, id, ip))
             
-            # Successor will most likely be consistent because of stabilize thread. So route to it
-            (suc_id, target_ip) = self.node.get_successor()
-            #print('[fix_finger] {}: Target CurrentID {} CurrentNodeIP {}'.format(self.node.id, id, target_ip))
-            (id, ip) = self.node.find_successor(id, target_ip)
-            self.node.ftable[self.next_finger] = (id, ip)
-            print('[fix_finger] {}: UpdatedID {} UpdatedNodeIP {}'.format(self.node.id, id, ip))
             print(self.node.ftable)
